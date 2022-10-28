99acd7b6f3c3a912105fc72669949ea55df88b48
Pedro Batista de Almeida Filho <pedro.filho@ambevtech.com.br>
2022-10-28 17:51:21 -0300

    first commit

---
+++ cypress/e2e/3-web/3-1-cadastro.cy.js
@@ -0,0 +1,82 @@
+/// <reference types = "cypress"/>
+
+const faker = require('faker-br');
+
+describe('Funcionalidade: Cadastro', () => {
+
+    beforeEach(() => {
+        cy.visit('/cadastrar');
+    });
+
+    it('Cadastro com sucesso', () => {
+        let nome = `${faker.name.firstName()} ${faker.name.lastName()}`;
+        let email = faker.internet.email(nome);
+        let senha = faker.internet.password();
+
+        cy.cadastro(nome, email, senha, senha);
+
+
+        cy.get('.large').should('contain', 'Dashboard');
+        cy.contains(nome).should('exist');
+
+    });
+
+    it('Devo validar a mensagem quando cadastrar com e-mail já existente', () => {
+        let nome = 'Augusto';
+        let email = faker.internet.email(nome);
+        let senha = 'qualquercoisa';
+        let confirmarSenha = 'qualquercoisa';
+
+        cy.cadastro(nome, email, senha, confirmarSenha);
+
+        cy.get('[data-test="navbar-logout"] > .hide-sm').click();
+
+        cy.cadastro(nome, email, senha, confirmarSenha);
+
+        cy.get('[data-test="alert"]').should('have.text', 'Usuário já registrado');
+        // cy.contains('Usuário já registrado').should('exist');
+    });
+
+});
+
+// describe('Funcionalidade: Cadastro', () => {
+//     context('Fluxos principais', () => { // Separar os contextos dos meus testes
+//         it('', () => {
+
+//         });
+//     });
+
+//     context('Fluxos alternativos', () => { // cenário de teste ou caso de teste ou história de teste
+
+//     });
+// });
+
+/*
+BDD
+    Funcionalidade: Cadastro
+
+    Cenário: Cadastro com sucesso
+    Dado que eu esteja na tela de cadastro
+    Quando eu preencher os campos obrigatórios
+    Então deve direcionar para a dashboard
+
+    Cenário: Cadastro com pessoa jurídica
+
+    Cenário: Cadastro com e-mail inválido
+
+HOOKS
+    Before (antes de todos cs cenários)
+    visit
+    login
+    input dados
+    criar conexao de banco de dados
+
+    Before each (antes de cada cenéario)
+
+    After (depois de todos os cenários)
+    Matar a conexão com o banco de dados
+
+    After each (depois de cada cenário)
+    gerar um screenshot - evidencia
+*/
+

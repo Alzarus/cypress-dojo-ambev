da412be82b97687d9489912e37bb1a067c3785b7
Pedro Batista de Almeida Filho <pedro.filho@ambevtech.com.br>
2022-11-10 00:05:23 -0300

    desafio 2 concluido

--- cypress/e2e/4-api/4-4-perfil.api.cy.js
+++ cypress/e2e/4-api/4-4-perfil.api.cy.js
@@ -41,28 +41,6 @@ describe('Funcionalidade: Perfil via api', () => {
         });
     });
 
-    it('[PUT] - Deve adicionar uma experiência profissional do usuário', () => {
-        const options = {
-            method: 'PUT',
-            url: '/api/profile/experience',
-            headers: {
-                Cookie: token
-            },
-            body: {
-                title: 'QA Especialist',
-                company: 'Ambev',
-                from: '2022-09-09'
-            }
-        };
-
-        cy.request(options).then(($response) => {
-            expect($response.status).to.equal(200);
-            expect($response.body.experience[0].title).to.equal('QA Especialist');
-            expect($response.body.experience[0].company).to.equal('Ambev');
-        });
-
-    });
-
     it('[POST] - Deve realizar cadastro de perfil do usuário', () => {
         // const testSkillsArray = testSkills.split(','); // string to array
         const testStatus = perfil.cargo;
@@ -139,24 +117,10 @@ describe('Funcionalidade: Perfil via api', () => {
         })
     });
 
-    //todo: continuar
-    it('[DELETE] - Deve realizar exclusão de experiência profissional', () => {
-        const optionsGet = {
-            method: 'GET',
-            url: '/api/profile',
-
-        }
-
-        // health check - garantir que esta funcionando
-        // contrato - garantir que o endpoint não teve seus atributos alterados
-        // aceitação - garantir que o endpoint funciona ou apresenta os resultados de falha esperados
-        // funcional - garantir que um conjunto de endpoints funcionam como na UI
-        // https://conexaoqa.herokuapp.com/api-docs/#/
-
-
-        const optionsDelete = {
-            method: 'DELETE',
-            url: `/api/profile/experience/${expId}`,
+    it('[PUT] - Deve adicionar uma experiência profissional do usuário', () => {
+        const options = {
+            method: 'PUT',
+            url: '/api/profile/experience',
             headers: {
                 Cookie: token
             },
@@ -166,6 +130,73 @@ describe('Funcionalidade: Perfil via api', () => {
                 from: '2022-09-09'
             }
         };
+
+        cy.request(options).then(($response) => {
+            expect($response.status).to.equal(200);
+            expect($response.body.experience[0].title).to.equal('QA Especialist');
+            expect($response.body.experience[0].company).to.equal('Ambev');
+        });
+
+    });
+
+    it('[DELETE] - Deve realizar exclusão de experiência profissional', () => {
+        const optionsGet = {
+            method: 'GET',
+            url: '/api/profile/me',
+            headers: {
+                Cookie: token
+            }
+        }
+
+        const getExpId = () => {
+            return cy.request(optionsGet).then((res) => {
+                //health check
+                expect(res.status).to.equal(200);
+                expect(res.duration).to.be.below(500);
+
+                //get expId
+                let arrLenght = res.body.experience.length;
+                return {
+                    id: res.body.experience[arrLenght - 1]._id.toString(),
+                    arrLenght: arrLenght
+                };
+            });
+        }
+
+        getExpId().then((obj) => {
+            const optionsDelete = {
+                method: 'DELETE',
+                url: `/api/profile/experience/${obj.id}`,
+                headers: {
+                    Cookie: token
+                }
+            };
+
+            cy.request(optionsDelete).then((res) => {
+                //heath check
+                expect(res.status).to.equal(200);
+                expect(res.duration).to.be.below(500);
+
+                //contrato
+                expect(res.body).to.include.keys("bio", "company", "date", "education", "experience",
+                    "githubusername", "location", "skills", "social", "status", "user", "website");
+
+                //aceitacao
+                expect(res.body.experience.length).to.not.equal(obj.arrLenght);
+
+                let errorFlag = false;
+                res.body.experience.forEach(element => {
+                    if (element._id == obj.id) {
+                        errorFlag = true;
+                    }
+                });
+
+                expect(errorFlag).to.be.false;
+
+            });
+
+        });
+
     });
 
     it('[PUT] - Deve realizar inclusão de formação acadêmica', () => {
@@ -176,11 +207,105 @@ describe('Funcionalidade: Perfil via api', () => {
                 Cookie: token
             },
             body: {
-                title: 'QA Especialist',
-                company: 'Ambev',
-                from: '2022-09-09'
+                school: 'IFBA',
+                degree: 'Superior',
+                fieldofstudy: 'Análise de sistemas',
+                from: '2017-01-01',
+                to: '2022-12-31',
+                current: false,
+                description: 'Um dos melhores cursos de ADS do país.'
             }
         };
+
+        cy.request(options).then((res) => {
+            //health check
+            expect(res.status).to.equal(200);
+            expect(res.duration).to.be.below(500);
+
+            //contrato
+            let arrLength = res.body.education.length;
+            expect(res.body.education[arrLength - 1]).to.include.keys("current", "degree", "description",
+                "fieldofstudy", "from", "school", "to", "_id");
+
+            //aceitacao
+            let lastEducation = res.body.education[arrLength - 1];
+            expect(lastEducation.current).to.equal(options.body.current);
+            expect(lastEducation.degree).to.equal(options.body.degree);
+            expect(lastEducation.description).to.equal(options.body.description);
+            expect(lastEducation.fieldofstudy).to.equal(options.body.fieldofstudy);
+            expect(lastEducation.from.substring(0, 10)).to.equal(options.body.from);
+            expect(lastEducation.school).to.equal(options.body.school);
+            expect(lastEducation.to.substring(0, 10)).to.equal(options.body.to);
+
+        });
+
+
+    });
+
+    it('[DELETE] - Deve realizar exclusão de formação acadêmica', () => {
+        const optionsGet = {
+            method: 'GET',
+            url: '/api/profile/me',
+            headers: {
+                Cookie: token
+            }
+        }
+
+        const getEduId = () => {
+            return cy.request(optionsGet).then((res) => {
+                //health check
+                expect(res.status).to.equal(200);
+                expect(res.duration).to.be.below(500);
+
+                //get eduId
+                let arrLenght = res.body.education.length;
+                return {
+                    id: res.body.education[arrLenght - 1]._id.toString(),
+                    arrLenght: arrLenght
+                };
+            });
+        }
+
+        getEduId().then((obj) => {
+            const optionsDelete = {
+                method: 'DELETE',
+                url: `/api/profile/education/${obj.id}`,
+                headers: {
+                    Cookie: token
+                }
+            };
+
+            cy.request(optionsDelete).then((res) => {
+                //heath check
+                expect(res.status).to.equal(200);
+                expect(res.duration).to.be.below(500);
+
+                //contrato
+                expect(res.body).to.include.keys("bio", "company", "date", "education", "experience",
+                    "githubusername", "location", "skills", "social", "status", "user", "website");
+
+                //aceitacao
+                expect(res.body.education.length).to.not.equal(obj.arrLenght);
+
+                let errorFlag = false;
+                res.body.education.forEach(element => {
+                    if (element._id == obj.id) {
+                        errorFlag = true;
+                    }
+                });
+
+                expect(errorFlag).to.be.false;
+
+            });
+
+        });
+
     });
 
 });
+
+        // health check - garantir que esta funcionando
+        // contrato - garantir que o endpoint não teve seus atributos alterados
+        // aceitação - garantir que o endpoint funciona ou apresenta os resultados de falha esperados
+        // funcional - garantir que um conjunto de endpoints funcionam como na UI
+        // https://conexaoqa.herokuapp.com/api-docs/#/
